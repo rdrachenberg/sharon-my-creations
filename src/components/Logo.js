d60d195c3ad9a90492ef6863f938853e05fa0966
Ryan Drachenberg <tssinvestments@gmail.com>
2023-05-24 14:24:07 -0400

    added custom logo and favicon

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Logo() {
     return (
         <Link href={'/'} className='flex items-center space-x-2'>
-            <Image src='/logo.png' width={42} height={42} style={{borderRadius:'10px', border: '1px solid'}} alt="Any Store logo" />
+            <Image src='/logo.png?v=1' width={52} height={52} style={{borderRadius:'10px', border: '1px solid'}} alt="Sharon My Creations logo" />
             <span className='hidden sm:inline-block font-extrabold text-3xl text-gray-700'>
                 Sharon My Creations
             </span>
