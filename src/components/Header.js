c834a8d081b675861a152b18582705272f5a6235
Ryan Drachenberg <tssinvestments@gmail.com>
2023-05-26 14:11:49 -0400

    changed logo size

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
 
     return (
         <header className='sticky top-0 bg-white z-10 shadow'> 
-            <div className='container mx-auto p-6 flex justify-between'>
+            <div className='container mx-auto p-2 flex justify-between'>
                 <Logo />
                 <Link href='/cart' className='flex items-center space-x-1 text-gray-700 hover:text-gray-900'>
                     <div className='relative'>
