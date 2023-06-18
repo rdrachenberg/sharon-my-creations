15ffa91d1ce2faabe24a9792589434e439384010
Ryan Drachenberg <tssinvestments@gmail.com>
2023-06-18 13:53:01 -0400

    made size adjutments to the About Me on the logo component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -11,7 +11,7 @@ export default function Logo() {
             </span>
         </Link>
         <Link href={'/about'}>
-            <span className='font-semibold lg:text-1xl pl-3 mx-auto my-auto hover:text-[#257fdf] max-md:text-xs'>About Me</span>
+            <span className='font-semibold text-1xl pl-3 mx-auto my-auto hover:text-[#257fdf] max-md:text-md'>About Me</span>
         </Link>
     </div>
     )
