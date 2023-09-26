bd725831efd51b82e4f8964f16247af02d70516d
Ryan Drachenberg <tssinvestments@gmail.com>
2023-09-26 12:01:57 -0400

    took site into maintenence mode. per client request and waiting on correct content from client

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import { Toaster } from 'react-hot-toast';
+import Image from 'next/image';
 import AppLayout from 'src/components/Layout'
 import 'src/styles/globals.css'
 import { CartProvider } from 'use-shopping-cart'
@@ -6,10 +7,21 @@ import { CartProvider } from 'use-shopping-cart'
 const stripeKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
 
 export default function App({ Component, pageProps }) {
+  const isInMaintenance = true;
+  const MainatenancePage = () => { 
+    return(
+      <div className='flex justify-center bg-slate-200 min-h-screen'>
+      
+      <div className='justify-center p-5 rounded-lg'> 
+        <Image src={'/coming-soon.jpg'} width={500} height={500} alt='coming soon' className='rounded-lg'/>
+      </div>
+      
+      
+      </div>)}
   return (
     <CartProvider stripe={stripeKey} cartMode='checkout-session' currency='USD'>
       <AppLayout>
-        <Component {...pageProps} />
+        {isInMaintenance ? <MainatenancePage /> : <Component {...pageProps} />}
         <Toaster />
       </AppLayout>
     </CartProvider>
